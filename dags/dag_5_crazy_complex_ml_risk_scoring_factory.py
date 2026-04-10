1fb6d24b0304f85efef91bad5eab6b4b645924e9
Joseph <jjuhn1119@gmail.com>
2026-04-10 09:19:09 -0400

    yaml not loading, added logging for investigation

--- dags/dag_5_crazy_complex_ml_risk_scoring_factory.py
+++ dags/dag_5_crazy_complex_ml_risk_scoring_factory.py
@@ -6,8 +6,12 @@ DEFAULT_CONFIG_ROOT_DIR = "/usr/local/airflow/dags/"
 CONFIG_ROOT_DIR = Path(os.getenv("CONFIG_ROOT_DIR", DEFAULT_CONFIG_ROOT_DIR))
 
 config_file = str(CONFIG_ROOT_DIR / "crazy_complex_ml_risk_scoring.yml")
+config_path = CONFIG_ROOT_DIR / "crazy_complex_ml_risk_scoring.yml"
 
-load_yaml_dags(
-    globals_dict=globals(),
-    config_filepath=config_file,
-)
+if config_path.exists():
+    load_yaml_dags(
+        globals_dict=globals(),
+        config_filepath=str(config_path),
+    )
+else:
+    print(f"Warning: Configuration file not found at {config_path}")
